62249069a8d9adb514071439c0e44a7ca530bb0b
Sakthivel Selvam <sakthiselvamcs@outlook.com>
2023-11-02 14:43:59 +0530

    Assessment Created

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TopNavigationComponent } from './top-navigation/top-navigation.component';
+import { LeftNavigationComponent } from './left-navigation/left-navigation.component';
+import { TopDataCardsComponent } from './top-data-cards/top-data-cards.component';
+import { NewUsersComponent } from './new-users/new-users.component';
+import { RecentOrdersComponent } from './recent-orders/recent-orders.component';
+import { GraphsComponent } from './graphs/graphs.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopNavigationComponent,
+    LeftNavigationComponent,
+    TopDataCardsComponent,
+    NewUsersComponent,
+    RecentOrdersComponent,
+    GraphsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/top-data-cards/top-data-cards.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-data-cards',
+  templateUrl: './top-data-cards.component.html',
+  styleUrls: ['./top-data-cards.component.scss']
+})
+export class TopDataCardsComponent {
+
+}
